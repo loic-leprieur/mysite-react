8d9b910a6c724cc846865b281ddde7ec9efe7d2c
Loïc LEPRIEUR <leprieurloic@gmail.com>
2021-02-19 19:51:58 +0100

    Ajouts des projets manquants

--- portofolio.php
+++ portofolio.php
@@ -9,28 +9,22 @@
                         <img src="/assets/img/basedocumentaireOI.png">
                         <span class="card-title">Intranet ASP.NET</span>
                     </div>
-                    <div class="card-content">
-                        <p>Intranet ASP.NET MVC 5 gérant la base documentaire de O-I Manufacturing Gironcourt</p>
-                    </div>
                     <div class="card-action">
                         <a href="https://github.com/loic-leprieur/OI-BaseDocumentaire">Dépot GitHub</a>
+                        <p>Intranet ASP.NET MVC 5 gérant la base documentaire de O-I Manufacturing Gironcourt</p>
                     </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col s12 m6">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/crazybox.png">
-                            <span class="card-title">CrazyBox</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Site web en PHP et jQuery pour la création de pochettes cadeaux. Réalisé lors du hackaton 'Crazy Charly Day' de l'IUT Charlemagne</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="https://vps.team-arma.fr/CrazyCharlyDay/">Lien vers le site web</a>
-                        </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/assets/img/crazybox.png">
+                        <span class="card-title">CrazyBox</span>
+                    </div>
+                    <div class="card-action">
+                        <a href="https://github.com/loic-leprieur/CrazyBox">Dépôt GitHub</a>
+                        <p>Site web en PHP et jQuery pour la création de pochettes cadeaux. Réalisé lors du hackaton 'Crazy Charly Day' de l'IUT Charlemagne</p>
                     </div>
                 </div>
             </div>
@@ -40,10 +34,48 @@
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/tchat-img.jpg">
-                            <span class="card-title">Application Xamarin de tchat</span>
                         </div>
-                        <div class="card-content">
+                        <div class="card-action">
+                            <span class="card-title">Application Xamarin de tchat</span>
                             <p>Composant d'une bibliothèque PCL pour créer un environnement de tchat (ex: Messenger)</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col s12 m6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="/assets/img/screenshot_app_TodoList.jpg">
+                        </div>
+                        <div class="card-action">
+                            <span class="card-title">Application Todo list Android</span>
+                            <p>Application utilisant une base de données locale pour stocker des tâches à accomplir dans une liste modifiable</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="/assets/img/screenshot_aventure_textuelle.jpg">
+                        </div>
+                        <div class="card-action">
+                            <span class="card-title">Jeu d'aventures textuelles</span>
+                            <p>Jeu réalisé avec un analyseur synthaxique et un éditeur de niveaux</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col s12 m6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="/assets/img/booku-appli-screenshot.gif">
+                        </div>
+                        <div class="card-action">
+                            <span class="card-title">Gestionnaire d'évenements à Book'U</span>
+                            <p>Application web pour gérer des événements comme des salons</p>
                         </div>
                     </div>
                 </div>
