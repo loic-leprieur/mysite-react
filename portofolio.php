35395e7f3048811e9e8e9dab4d1f86f8628d494e
Loïc LEPRIEUR <leprieurloic@gmail.com>
2021-02-21 15:17:13 +0100

    Réorganisation des blocks du portofolio

--- portofolio.php
+++ portofolio.php
@@ -3,7 +3,7 @@
 
         <div class="row">
         
-            <div class="col s12 m6">
+            <div class="col s12 m3">
                 <div class="card">
                     <div class="card-image">
                         <img src="/assets/img/basedocumentaireOI.png">
@@ -16,7 +16,7 @@
                 </div>
             </div>
 
-            <div class="col s12 m6">
+            <div class="col s12 m3">
                 <div class="card">
                     <div class="card-image">
                         <img src="/assets/img/crazybox.png">
@@ -29,8 +29,8 @@
                 </div>
             </div>
             
-            <div class="row">
-                <div class="col s12 m6">
+
+                <div class="col s12 m3">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/tchat-img.jpg">
@@ -42,33 +42,36 @@
                     </div>
                 </div>
 
-                <div class="col s12 m6">
+                <div class="col s12 m3">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/screenshot_app_TodoList.jpg">
                         </div>
                         <div class="card-action">
-                            <span class="card-title">Application Todo list Android</span>
+                            <p class="card-title">Application Todo list Android</p>
+                            <a href="https://github.com/loic-leprieur/TodoList">Dépôt GitHub</a>
                             <p>Application utilisant une base de données locale pour stocker des tâches à accomplir dans une liste modifiable</p>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
             <div class="row">
-                <div class="col s12 m6">
+                <div class="col s12 m4">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/screenshot_aventure_textuelle.jpg">
                         </div>
                         <div class="card-action">
-                            <span class="card-title">Jeu d'aventures textuelles</span>
+                            <p class="card-title">Jeu d'aventures textuelles</p>
+                            <a href="https://github.com/loic-leprieur/jeu-aventure-textuel">Dépôt GitHub</a>
                             <p>Jeu réalisé avec un analyseur synthaxique et un éditeur de niveaux</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col s12 m6">
+                <div class="col s12 m4">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/booku-appli-screenshot.gif">
@@ -76,6 +79,19 @@
                         <div class="card-action">
                             <span class="card-title">Gestionnaire d'évenements à Book'U</span>
                             <p>Application web pour gérer des événements comme des salons</p>
+                        </div>
+                    </div>
+                </div>
+              
+              <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="/assets/img/screenshot_appli_ShopNCook.jpg">
+                        </div>
+                        <div class="card-action">
+                            <p class="card-title">Application Shop&amp;Cook</p>
+                          	<a href="https://github.com/loic-leprieur/shop-and-cook">Dépôt GitHub</a>
+                            <p>Application Android de recherche de recette via une API et d'enregistrements d'ingrédients</p>
                         </div>
                     </div>
                 </div>
