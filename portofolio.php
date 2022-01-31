4baa37ab7ca54e8802cc907459903b23a1fab3b5
Loïc LEPRIEUR <leprieurloic@gmail.com>
2022-02-01 00:49:46 +0100

    Refonte du portofolio (rajout de texte et ajustement de l'affichage)

--- portofolio.php
+++ portofolio.php
@@ -3,7 +3,7 @@
 
         <div class="row">
         
-            <div class="col s12 m3">
+            <div class="col s12 m4">
 
             <div class="card">
                         <div class="card-image">
@@ -18,16 +18,16 @@
                 <
             </div>
 
-            <div class="col s12 m3">
+            <div class="col s12 m4">
                 <div class="card">
                     <div class="card-image">
                         <img src="/assets/img/crazybox.png">
-                        <span class="card-title">CrazyBox</span>
                     </div>
                     <div class="card-action">
+                        <span class="card-title">CrazyBox</span><br/><br/>
                         <a href="https://github.com/loic-leprieur/CrazyBox">Dépôt GitHub</a>
                         <p>Site web en PHP et jQuery pour la création de pochettes cadeaux. Réalisé lors du hackaton 'Crazy Charly Day' de l'IUT Charlemagne</p>
-                        <video controls loop autoplay preload width="300px">
+                        <video controls loop autoplay preload width="250px">
                             <source src="/assets/video/demo-crazybox.mp4" type="video/mp4">
                             Sorry, your browser doesn't support embedded videos.
                         </video>
@@ -36,27 +36,14 @@
             </div>
             
 
-                <div class="col s12 m3">
+                <div class="col s12 m4">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/tchat-img.jpg">
                         </div>
                         <div class="card-action">
                             <span class="card-title">Application Xamarin de tchat</span>
-                            <p>Composant d'une bibliothèque PCL pour créer un environnement de tchat (ex: Messenger)</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col s12 m3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/screenshot_app_TodoList.jpg">
-                        </div>
-                        <div class="card-action">
-                            <p class="card-title">Application Todo list Android</p>
-                            <a href="https://github.com/loic-leprieur/TodoList">Dépôt GitHub</a>
-                            <p>Application utilisant une base de données locale pour stocker des tâches à accomplir dans une liste modifiable</p>
+                            <p>Composant UI/UX dans une bibliothèque PCL (avec d'autres modules génériques) pour créer un environnement de tchat (ex: Messenger)</p>
                         </div>
                     </div>
                 </div>
@@ -64,7 +51,21 @@
         </div>
 
             <div class="row">
-                <div class="col s12 m4">
+
+                <div class="col s12 m3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="/assets/img/screenshot _app_TodoList.jpg">
+                        </div>
+                        <div class="card-action">
+                            <p class="card-title">Application Todo list Android</p>
+                            <a href="https://github.com/loic-leprieur/TodoList">Dépôt GitHub</a>
+                            <p>Application utilisant une base de données locale pour stocker des tâches à accomplir dans une liste modifiable. Implémentation de CRUD et de contrôles UI sur les smartphones Android.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col s12 m3">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/screenshot_aventure_textuelle.jpg">
@@ -72,32 +73,44 @@
                         <div class="card-action">
                             <p class="card-title">Jeu d'aventures textuelles</p>
                             <a href="https://github.com/loic-leprieur/jeu-aventure-textuel">Dépôt GitHub</a>
-                            <p>Jeu réalisé avec un analyseur synthaxique et un éditeur de niveaux</p>
+                            <p>Jeu d'aventures textuelles réalisé avec un analyseur synthaxique et un éditeur de niveaux.
+                                Le principe repose sur les jeux des années 80's où l'on saisissait du texte pour exécuter
+                                des actions et dont la problématique reposait sur les commandes comprises par l'ordinateur et 
+                                l'enchaînement des puzzles avec les objets et les éléments du décor. Application réalisée avec
+                                Java FX en version 8.
+                            </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col s12 m4">
+                <div class="col s12 m3">
                     <div class="card">
                         <div class="card-image">
                             <img src="/assets/img/booku-appli-screenshot.gif">
                         </div>
                         <div class="card-action">
                             <span class="card-title">Gestionnaire d'évenements à Book'U</span>
-                            <p>Application web pour gérer des événements comme des salons</p>
+                            <p>Application web Angular pour gérer des événements comme des salons. Réalisé avec la méthodologie SCRUM au sein
+                                d'une équipe de 3 autres étudiants flammands à Courtrai (Belgique) pour la start-up <strong>Book'u</strong> afin de
+                                tester la technologie. (Une application mobile avec Ionic avait aussi été réalisé en parallèle) 
+                            </p>
                         </div>
                     </div>
                 </div>
               
-              <div class="col s12 m4">
+              <div class="col s12 m3">
                 <div class="card">
                     <div class="card-image">
                         <img src="/assets/img/basedocumentaireOI.png">
-                        <span class="card-title">Intranet ASP.NET</span>
                     </div>
                     <div class="card-action">
+                        <span class="card-title">Intranet ASP.NET</span></br>
                         <a href="https://github.com/loic-leprieur/OI-BaseDocumentaire">Dépot GitHub</a>
-                        <p>Intranet ASP.NET MVC 5 gérant la base documentaire de O-I Manufacturing Gironcourt</p>
+                        <p>Pour remplacer une application sous Microsoft Access difficile à maintenir. Lors d'un stage à O-I
+                            Manufacturing (verrerie à Gironcourt-sur-Vraine) j'ai réalisé un site web sur l'intranet de l'entreprise en ASP.NET MVC 5 gérant la base documentaire.
+                            Il était possible de récupérer les documents du serveur Windows et les afficher sur le site dans un tableau triable, filtrable et avec une barre de recherche, la base de données
+                            sous SQL Server a permis de catégoriser les documents à partir de leur nom.
+                        </p>
                     </div>
                 </div>
               </div>
