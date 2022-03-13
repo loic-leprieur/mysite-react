b8090657edccddf1aec32f3c7ab84ebd35043775
loicleprieur <donleprieur@outlook.fr>
2022-03-13 23:37:47 +0100

    Màj de Infos et ajout d'une image

--- infos.php
+++ infos.php
@@ -1,26 +1,27 @@
 <?php require 'header.php';?>
 
 <div class="container min-container-space">
-	<div class="card indigo darken-4 white-text">
-		<div class="card-title center-align ">
+	<div class="card indigo darken-4 white-text" style="height: 100%; text-align: center;">
 			<h4>Informations</h4>
-		</div>
-		<div class="card-content">
 			<p>
-				Je suis un développeur junior curieux et intéressé par les
-				nouvelles technologies je travaille aujourd'hui avec 2s2i Solutions
-				en tant que consultant pour KP1.
+				Je suis un développeur junior mais curieux et intéressé par les
+				nouvelles technologies, je suis toujours dédié à 100% à ma tâche et 
+				je suis une personne passionné par ce qu'il fait et aime le partager.
 			</p>
 			<p>
-				Je travaille actuellement sur l'amélioration des logiciels propriétaires 2D/3D de
-				CAO (en Java et C#) dans le contexte métier du bâtiment.
+				A la recherche d'un nouvel emploi, je suis disponible immédiatement pour
+				une nouvelle opportunité qui saura m'apporter expérience et relations enrichissantes. 
+				Je recherche un poste de développeur web fullstack en ASP.NET ou PHP.
 			</p>
 			<ul>
 				<li>Email: <a href="mailto:contact@loic-leprieur.fr?subject=Site web (Infos)">contact@loic-leprieur.fr</a></li>
 				<li><a href="https://www.linkedin.com/in/loic-leprieur-1705/">Linkedin</a></li>
 			</ul>
+			<img src="/assets/img/fond-ordinateur-mug.jpg" style="width: 100%;">
 		</div>
 	</div>
+
+	
 </div>
 
 <?php require 'footer.php';?>
