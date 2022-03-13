6ac662a42eae80b1bcb41241f04a15d6d41b6493
loicleprieur <donleprieur@outlook.fr>
2022-03-13 23:35:38 +0100

    Rajout de validation du formulaire et ajout d'un champ emai

--- contact.php
+++ contact.php
@@ -1,9 +1,12 @@
 <?php
     $expediteur = $_POST['expediteur'];
     $message = $_POST['message'];
+    $email = $_POST['email'];
 
     // Connect to MySQL
-    $mysqli = mysqli_connect('db5000107891.hosting-data.io', 'dbu320355', 'Jwr1runevwhr;', 'dbs102408');
+
+    include('./dbconfig/config.php');
+    $mysqli = mysqli_connect($config['DB_HOST'], $config['DB_USERNAME'], $config['DB_PASSWORD'], $config['DB_DATABASE']);
 
     // Check our connection 
     if($mysqli->connect_error){
@@ -13,103 +16,153 @@
     //Create the table
     $sql1 = "CREATE TABLE IF NOT EXISTS `message`
     (id INT PRIMARY KEY NOT NULL,
-    expediteur  VARCHAR(100),
+    expediteur VARCHAR(100),
     message VARCHAR (800),
-    date DATE)";
+    date DATE,
+    email VARCHAR(254));";
     $insert1 = $mysqli->query($sql1);
 
     $sqlMaxId = "SELECT MAX(Id) FROM message";
     $maxId = $mysqli->query($sqlMaxId);
 
     //Insert our data
-    if(isset($expediteur) && isset($message)){
-        $newId = mysqli_fetch_array($maxId)[0] + 1;
-        $sql2 = "INSERT INTO message (id, expediteur, message, date) VALUES ($newId, '$expediteur', '$message', NOW())";
-        $insert2 = $mysqli->query($sql2);
+    if(!empty($expediteur) && !empty($message) && !empty($email)){
 
-        // Print response from MySQL
-        if($insert2) {
-            echo '<div class="card blue-grey darken-1" style="margin:0;">Votre message a bien été envoyé</div>';
-            mail('leprieurloic@gmail.com', 'Message de leprieur-loic.fr', "De ".$expediteur."\n Message:\n".$message);
+        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+        
+            $secretAPIkey = '6LeCU9keAAAAACkwRmSieGjdjNlRlgcSIFes4Q7c';
+
+            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretAPIkey.'&response='.$_POST['g-recaptcha-response']);
+                
+            $response = json_decode($verifyResponse);
+            if($response->success){
+                $newId = mysqli_fetch_array($maxId)[0] + 1;
+                $sql2 = "INSERT INTO message (id, expediteur, message, date, email) VALUES ($newId, '$expediteur', '$message', NOW(), '$email')";
+
+                $insert2 = $mysqli->query($sql2);
+
+                // Send email after SQL query
+                if($insert2) {
+                    mail('leprieurloic@gmail.com', 'Message de loic-leprieur.fr', "De ".$expediteur."\nMessage:\n".$message);
+                } else {
+                    die("Error: ($mysqli->errno) : {$mysqli->error}");
+                }
+            } else {
+                $response = array(
+                    "status" => "alert-danger",
+                    "message" => "Robot verification failed, please try again."
+                );
+            }
         } else {
-            die("Error: ($mysqli->errno) : {$mysqli->error}");
+            
+            
         }
-    } 
-       
-    
+    }
 
     // Close our connection
     mysqli_close($mysqli);
 ?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="assets\css\materialize.min.css">
-    <link rel="stylesheet" href="assets\css\styles.css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="favicon.png" />
-    
-    <!-- Compiled and minified JavaScript -->
-    <script src="assets\js\jquery-3.3.1.min.js"></script>
-    <script src="assets\js\materialize.min.js"></script>
-    <script src="assets\js\main.js"></script>
+<?php require 'header.php';?>
 
-    <title>Blog de Loïc</title>
-</head>
-<body>
-    <!-- Navigation section -->
-    <nav>
-        <div class="nav-wrapper deep-purple darken-3">
-          <a href="#!" class="brand-logo"><span class="logo grey-text text-lighten-2">Blog de Loïc LEPRIEUR</span></a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="/">Accueil</a></li>
-            <li><a href="/portofolio.php">Portofolio</a></li>
-            <li><a href="#">Infos</a></li>
-            <li><a href="/contact.php">Contact</a></li>
-          </ul>
-        </div>
-        <ul class="sidenav" id="mobile-demo">
-            <li><a href="/">Accueil</a></li>
-            <li><a href="/portofolio.php">Portofolio</a></li>
-            <li><a href="#">Infos</a></li>
-            <li><a href="/contact.php">Contact</a></li>
-        </ul>
-    </nav>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+    <script>
+        $("#btnSubmit").on("click", function(){
+            var $this = $(this);
+            var $caption = $this.html();
+            var form = "#contact-form";
+            var formData = $(form).serializeArray();
+            var route = $(form).attr('action');
+
+            $.ajax({
+                type: POST,
+                url: route,
+                data: formData,
+                beforeSend: function() {
+                    $this.attr('disabled', true).html('Processing...');
+                },
+                success: function(response) {
+                    response = JSON.parse(response);
+
+                    validateForm();
+
+                    // Check if there is has_error property on json response from the server
+                    if(!response.hasOwnProperty('has_error')) {
+                        // Reload lists of employees
+                        all();
+
+                        // We will display the result using alert
+                        Swal.fire({
+                        icon: 'success',
+                        title: 'Success.',
+                        text: response.response
+                        });
+
+                        // Reset form
+                        resetForm(form);
+                    } else {
+                        // We will display the result using alert
+                        validationForm("#contact-form", response.errors);
+                    }
+
+                    $this.attr('disabled', false).html($caption);
+	            
+                    },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    // You can put something here if there is an error from submitted request
+                }
+            });
+        });
+
+        function validateForm() {
+            var emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            if(!emailReg.test(contact-form.email.value)){
+                $('#alert-message').css('display', 'block');
+                $('#alert-message').removeClass('alert-warning');
+                $('#alert-message').addClass('alert-success');
+                $('#alert-message').text('Email invalide');
+                alert('email incorrect');
+                return false;
+            } else {
+                if ($('#message').value.indexOf(';') =! -1) {
+                    $('#alert-message').css('display', 'block');
+                    $('#alert-message').removeClass('alert-success');
+                    $('#alert-message').addClass('alert-warning');
+                    $('#alert-message').text('Votre message n\'est pas correct');
+                    alert('message incorrect');
+                    return false;
+                } else {
+                    $('#alert-message').css('display', 'block');
+                    $('#alert-message').removeClass('alert-warning');
+                    $('#alert-message').addClass('alert-success');
+                    $('#alert-message').text('Votre message a bien été envoyé');
+                    alert('message envoyé');
+                    return true;
+                }
+            }
+        }
+    </script>
 
     <div class="parallax-container">
-        <div class="container">
+        <div class="main container">
+            <div id="alert-message" class="alert" style="display:none;"></div>
             <div class="row">
-                <form method="post" action="contact.php">
-                <div class="input-field">
-                    <input type="text" name="expediteur" placeholder="Votre nom" style="color: #FFFF; background-color: rgba(129,140,143,.6);"; />
-                    <textarea name="message" placeholder="Entrez votre message ici" rows="40" cols"50" style="height:200px; color: #FFFF; background-color: rgba(129,140,143,.6);"></textarea>
-                    <br>
-                    <input class="waves-effect waves-light btn" type="submit" value="Envoyer" style="color:#FFFF;">
-                </div>
+                <form method="POST" action="" id="contact-form" name="contact-form" onsubmit="validateForm()">
+                    <div class="input-field">
+                        <input type="text" name="expediteur" placeholder="Votre nom" style="color: #FFFF; background-color: rgba(129,140,143,.6);"; />
+                        <input type="text" name="email" placeholder="Votre email" style="color: #FFFF; background-color: rgba(129,140,143,.6);"; />
+                        <textarea id="message" name="message" placeholder="Entrez votre message ici" rows="40" cols="50" style="height:200px; color: #FFFF; background-color: rgba(129,140,143,.6);"></textarea>
+                        <br>
+                        <div class="g-recaptcha" data-sitekey="6LeCU9keAAAAAKv6LbiviEfv9t2IsHsOMbhmnzov"></div>
+                        <br>
+                        <input id="btnSubmit" class="waves-effect waves-light btn" type="submit" value="Envoyer" style="color:#FFFF; background-color:#ffab00;">
+                    </div>
                 </form>
             </div>
         </div>
         <div class="parallax"><img src="assets\img\programing-photo.jpg"></div>
-       <!--<div class="parallax"><img src="assets\img\backpack.jpeg"></div>-->
-    </div>
-    <!-- Content -->                                                                                                                                
+    </div>                                                                                                                             
 
-    <script>
-        $(document).ready(function(){
-            $('.sidenav').sidenav();
-            $('.parallax').parallax();
-            $('.carousel').carousel();
-        });
-    </script>
-              
-</body>
-</html>
+<?php require 'footer.php';?>
