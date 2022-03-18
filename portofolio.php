870eecbc6aa1ca356f61e72b1ecd8e60524df6b4
loicleprieur <donleprieur@outlook.fr>
2022-03-18 20:23:03 +0100

    Changement de structure des cards pour les afficher plus clairement

--- portofolio.php
+++ portofolio.php
@@ -3,31 +3,30 @@
 
         <div class="row">
         
-            <div class="col s12 m4">
+            <div class="col s12 m6 l4">
 
-            <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/screenshot_appli_ShopNCook.jpg">
-                        </div>
-                        <div class="card-action">
-                            <p class="card-title">Application Shop&amp;Cook</p>
-                          	<a href="https://github.com/loic-leprieur/shop-and-cook">Dépôt GitHub</a>
-                            <p>Application Android de recherche de recette via une API et d'enregistrements d'ingrédients</p>
-                        </div>
-                    </div>
-                <
-            </div>
-
-            <div class="col s12 m4">
                 <div class="card">
                     <div class="card-image">
-                        <img src="/assets/img/crazybox.png">
+                        <img data-fancybox src="/assets/img/screenshot_appli_ShopNCook.jpg">
+                    </div>
+                    <div class="card-action">
+                        <p class="card-title">Application Shop&amp;Cook</p>
+                        <a href="https://github.com/loic-leprieur/shop-and-cook">Dépôt GitHub</a>
+                        <p>Application Android de recherche de recette via une API et d'enregistrements d'ingrédients</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m6 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img data-fancybox src="/assets/img/crazybox.png">
                     </div>
                     <div class="card-action">
                         <span class="card-title">CrazyBox</span><br/><br/>
                         <a href="https://github.com/loic-leprieur/CrazyBox">Dépôt GitHub</a>
                         <p>Site web en PHP et jQuery pour la création de pochettes cadeaux. Réalisé lors du hackaton 'Crazy Charly Day' de l'IUT Charlemagne</p>
-                        <video controls loop autoplay preload width="250px">
+                        <video data-fancybox href="/assets/video/demo-crazybox.mp4" controls loop autoplay preload width="250px">
                             <source src="/assets/video/demo-crazybox.mp4" type="video/mp4">
                             Sorry, your browser doesn't support embedded videos.
                         </video>
@@ -35,73 +34,69 @@
                 </div>
             </div>
             
-
-                <div class="col s12 m4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/tchat-img.jpg">
-                        </div>
-                        <div class="card-action">
-                            <span class="card-title">Application Xamarin de tchat</span>
-                            <p>Composant UI/UX dans une bibliothèque PCL (avec d'autres modules génériques) pour créer un environnement de tchat (ex: Messenger)</p>
-                        </div>
+            <div class="col s12 m6 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img data-fancybox src="/assets/img/tchat-img.jpg">
+                    </div>
+                    <div class="card-action">
+                    <span class="card-title">Application Xamarin de tchat</span>
+                    <p>Composant UI/UX dans une bibliothèque PCL (avec d'autres modules génériques) pour créer un environnement de tchat (ex: Messenger)</p>
                     </div>
                 </div>
             </div>
-        </div>
 
-            <div class="row">
 
-                <div class="col s12 m3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/screenshot _app_TodoList.jpg">
-                        </div>
-                        <div class="card-action">
-                            <p class="card-title">Application Todo list Android</p>
-                            <a href="https://github.com/loic-leprieur/TodoList">Dépôt GitHub</a>
-                            <p>Application utilisant une base de données locale pour stocker des tâches à accomplir dans une liste modifiable. Implémentation de CRUD et de contrôles UI sur les smartphones Android.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col s12 m3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/screenshot_aventure_textuelle.jpg">
-                        </div>
-                        <div class="card-action">
-                            <p class="card-title">Jeu d'aventures textuelles</p>
-                            <a href="https://github.com/loic-leprieur/jeu-aventure-textuel">Dépôt GitHub</a>
-                            <p>Jeu d'aventures textuelles réalisé avec un analyseur synthaxique et un éditeur de niveaux.
-                                Le principe repose sur les jeux des années 80's où l'on saisissait du texte pour exécuter
-                                des actions et dont la problématique reposait sur les commandes comprises par l'ordinateur et 
-                                l'enchaînement des puzzles avec les objets et les éléments du décor. Application réalisée avec
-                                Java FX en version 8.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col s12 m3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="/assets/img/booku-appli-screenshot.gif">
-                        </div>
-                        <div class="card-action">
-                            <span class="card-title">Gestionnaire d'évenements à Book'U</span>
-                            <p>Application web Angular pour gérer des événements comme des salons. Réalisé avec la méthodologie SCRUM au sein
-                                d'une équipe de 3 autres étudiants flammands à Courtrai (Belgique) pour la start-up <strong>Book'u</strong> afin de
-                                tester la technologie. (Une application mobile avec Ionic avait aussi été réalisé en parallèle) 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-              
-              <div class="col s12 m3">
+            <div class="col s12 m6 l4">
                 <div class="card">
                     <div class="card-image">
-                        <img src="/assets/img/basedocumentaireOI.png">
+                        <img data-fancybox src="/assets/img/screenshot _app_TodoList.jpg">
+                    </div>
+                    <div class="card-action">
+                        <p class="card-title">Application Todo list Android</p>
+                        <a href="https://github.com/loic-leprieur/TodoList">Dépôt GitHub</a>
+                        <p>Application utilisant une base de données locale pour stocker des tâches à accomplir dans une liste modifiable. Implémentation de CRUD et de contrôles UI sur les smartphones Android.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m6 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img data-fancybox src="/assets/img/screenshot_aventure_textuelle.jpg">
+                    </div>
+                    <div class="card-action">
+                        <p class="card-title">Jeu d'aventures textuelles</p>
+                        <a href="https://github.com/loic-leprieur/jeu-aventure-textuel">Dépôt GitHub</a>
+                        <p>Jeu d'aventures textuelles réalisé avec un analyseur synthaxique et un éditeur de niveaux.
+                            Le principe repose sur les jeux des années 80's où l'on saisissait du texte pour exécuter
+                            des actions et dont la problématique reposait sur les commandes comprises par l'ordinateur et 
+                            l'enchaînement des puzzles avec les objets et les éléments du décor. Application réalisée avec
+                            Java FX en version 8.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m6 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img data-fancybox src="/assets/img/booku-appli-screenshot.gif">
+                    </div>
+                    <div class="card-action">
+                        <span class="card-title">Gestionnaire d'évenements à Book'U</span>
+                        <p>Application web Angular pour gérer des événements comme des salons. Réalisé avec la méthodologie SCRUM au sein
+                            d'une équipe de 3 autres étudiants flammands à Courtrai (Belgique) pour la start-up <strong>Book'u</strong> afin de
+                            tester la technologie. (Une application mobile avec Ionic avait aussi été réalisé en parallèle) 
+                        </p>
+                    </div>
+                </div>
+            </div>
+              
+            <div class="col s12 m12 l12">
+                <div class="card">
+                    <div class="card-image">
+                        <img data-fancybox src="/assets/img/basedocumentaireOI.png">
                     </div>
                     <div class="card-action">
                         <span class="card-title">Intranet ASP.NET</span></br>
@@ -113,11 +108,8 @@
                         </p>
                     </div>
                 </div>
-              </div>
             </div>
-
         </div>
-        
     </div>
 
 <?php require 'footer.php';?>
