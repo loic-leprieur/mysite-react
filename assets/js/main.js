2398ec0b7cba1f9fcb4ee3035f3c336bbaa0f837
loicleprieur <donleprieur@outlook.fr>
2022-06-28 22:18:37 +0200

    Ajout d'animations et menu sticky géré

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,45 @@ $(document).ready(function(){
 		fullWidth: true,
 		indicators: true
 	});
-	//console.log('ok');
+
+
+	var opacity = 0;
+	var intervalID = 0;
+	window.onload = fadeIn;
+
+	function fadeIn() {
+		setInterval(show, 200);
+	}
+
+	function show() {
+		var banner = document.getElementById("banner-profile");
+		opacity = Number(window.getComputedStyle(banner)
+							.getPropertyValue("opacity"));
+		if (opacity < 1) {
+			opacity = opacity + 0.1;
+			banner.style.opacity = opacity
+		} else {
+			clearInterval(intervalID);
+		}
+
+		$('#mail-button').effect("bounce", { direction:'down', times:8 }, 300);
+	}
+
+	window.onscroll = function() {addStickyToNav()};
+
+	// Get the navbar
+	var navbar = document.getElementById("navbar");
+
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
+
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function addStickyToNav() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	} else {
+		navbar.classList.remove("sticky");
+	}
+}
+
 });
