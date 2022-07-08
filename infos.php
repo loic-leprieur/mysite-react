01d02e8254dfce7b778032e2d6a8408bf8e277d0
loicleprieur <donleprieur@outlook.fr>
2022-07-09 00:43:34 +0200

    Refonte de infos, plus de texte mieux formaté

--- infos.php
+++ infos.php
@@ -1,20 +1,24 @@
-<section id="infos">
-<h3 style="color:#FF8F00;">Informations</h3>
-	<div class="card white darken-4 black-text" style="height: 100%; text-align: center; display:block;">
-			<p>
-				Je suis un développeur junior mais curieux et intéressé par les
-				nouvelles technologies, je suis toujours dédié à 100% à ma tâche et 
-				je suis une personne passionné par ce qu'il fait et aime le partager.
-			</p>
-			<p>
-				A la recherche d'un nouvel emploi, je suis disponible immédiatement pour
-				une nouvelle opportunité qui saura m'apporter expérience et relations enrichissantes. 
-				Je recherche un poste de développeur web fullstack en ASP.NET ou PHP.
-			</p>
-			<ul>
-				<li>Email: <a href="mailto:contact@loic-leprieur.fr?subject=Site web (Infos)">contact@loic-leprieur.fr</a></li>
-				<li><a href="https://www.linkedin.com/in/loic-leprieur-1705/">Linkedin</a></li>
-			</ul
-		</div>
+<section id="infos" class="section">
+	<h3 style="color:#FF8F00;">Informations</h3>
+	<div class="card white darken-4 black-text" style="height: 100%; text-align: left; display:block; padding: 0% 2% 0% 2%;">
+		<p>
+			Je suis certes un développeur junior (~ 2 ans) mais curieux et intéressé par les
+			nouvelles technologies, je suis toujours dédié à 100% à ma tâche et
+			je suis une personne passionné qui prend plaisir à travailler dans son domaine.
+			<br /><br />Au cours de mes expériences professionnelles j'ai eu la chance de travailler dans des secteurs et des environements techniques variés du BTP à l'assurance vie et du Java SE client lourd pour des plans au CRM en ASP.NET Core.
+			<br /><br />J'apprends vite et je continue de me former à de nouvelles technolgies ou techniques.
+			Proactif, force de proposition et autonome j'ai tendance à approcher un projet par son achitecture en l'analysant via des diagrammes puis à ajouter ou modifier une feature jusqu'à pleinement comprendre la base de code sur le bout des doigts.
+			<br /><br />En dehors du travail et de la programmation je m'intéresse à l'astronomie, je fais des lives sur Twitch en tant que streamer et je lis beaucoup de livres policier et fantastiques (The Witcher et les romans d'Agatha Christie ❤️)
+		</p>
+		<p class="center-align">
+			A la recherche d'un nouvel emploi, je suis disponible immédiatement pour
+			une nouvelle opportunité qui saura m'apporter expériences et relations enrichissantes.
+			<br /><br />💼 Je recherche un poste de <strong>développeur web backend ou fullstack en ASP.NET ou PHP</strong> 💼.
+		</p>
+		<ul class="center-align">
+			<li><a href="mailto:contact@loic-leprieur.fr?subject=Site web (Infos)">contact@loic-leprieur.fr</a></li>
+			<li><a href="https://www.linkedin.com/in/loic-leprieur-1705/">Linkedin</a></li>
+			<li><a href="#contact">Formulaire de contact</li>
+		</ul>
 	</div>
 </section>
