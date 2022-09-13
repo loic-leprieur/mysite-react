e0906a111a5ff38fe28c7d77c329167df086d39f
Loïc LEPRIEUR <leprieurloic@gmail.com>
2022-09-13 21:58:52 +0200

    Ajout d'une description pour le SEO

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
+	<meta name="description" content="Site personnel de Loïc LEPRIEUR, présentation du CV, des projets informatiques réalisés et des expériences passées en ASP.NET, Java, JavaScript et PHP." />
+	
 	<!-- Compiled and minified CSS -->
 	<link rel="stylesheet" href="\assets\css\materialize.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -15,6 +16,9 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
 	<link rel="stylesheet" href="\assets\css\styles.css" />
+
+	<!-- Apple webclip -->
+	<link rel="apple-touch-icon" href="\assets\img\touch-icon-iphone.png" />
 
 	<!-- JS -->
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -31,16 +35,16 @@
 	<!-- Navigation section -->
 	<nav id="navbar">
 		<div class="nav-wrapper white darken-4">
-			<span class="align-center-small"> <a href="/" class="brand-logo"> <span class="logo">L</span><span class="logo-text">oïc LEPRIEUR</span>
+			<span class="align-center-small"> <a href="#" class="brand-logo"> <span class="logo">L</span><span class="logo-text">oïc LEPRIEUR</span>
 				</a> <a href="#" data-target="mobile-demo" class="sidenav-trigger"> <i class="material-icons">menu</i>
 				</a>
 			</span>
 			<ul class="right hide-on-med-and-down">
-				<li><a href="#accueil">Accueil</a></li>
+				<li><a id="link-home" href="#accueil">Accueil</a></li>
 				<li><a href="../blog">Blog</a></li>
-				<li><a href="#portfolio">Portfolio</a></li>
-				<li><a href="#infos">Infos</a></li>
-				<li><a href="#contact">Contact</a></li>
+				<li><a id="link-portfolio" href="#portfolio">Portfolio</a></li>
+				<li><a id="link-infos" href="#infos">Infos</a></li>
+				<li><a id="link-contact" href="#contact">Contact</a></li>
 			</ul>
 		</div>
 		<ul class="sidenav white darken-4" id="mobile-demo">
