0ca1b194af87f6b93165bbf42a0c87f365bfdaa8
Loïc LEPRIEUR <leprieurloic@gmail.com>
2023-01-04 23:47:09 +0100

    Utilisation de axios pour la requête HTTP POST (action Contact)

--- mysite_react/src/Contact.js
+++ mysite_react/src/Contact.js
@@ -1,8 +1,38 @@
 import React from "react";
 import clavier from "./img/programing-photo.jpg";
 import $ from 'jquery';
-import axios from 'axios';
-import { useState } from "react";
+
+function onFormSubmit() {
+
+    var $this = $(this);
+    //var $caption = $this.html();
+    var form = "#contact-form";
+    var formData = $(form).serializeArray();
+
+    var request = $.ajax({
+        type: 'POST',
+        url: "contact.php",
+        data: formData,
+        beforeSend: function() {
+            $this.attr('disabled', true).html('Chargement...');
+            if(request != null && !validateForm()){
+                request.abort();
+            }
+        },
+        success: function(response) {
+            response = JSON.parse(response);
+
+            $('#alert-message').css('display', 'block');
+            $('#alert-message').removeClass('alert-warning');
+            $('#alert-message').addClass('alert-success');
+            $('#alert-message').text('Votre message a bien été envoyé');
+            
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            // You can put something here if there is an error from submitted request
+        }
+    });
+}
 
 function isValidEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -25,78 +55,35 @@ function validateForm() {
             $('#alert-message').text('Votre message n\'est pas correct');
             return false;
         } else {
-            $('#alert-message').css('display', 'block');
-            $('#alert-message').removeClass('alert-warning');
-            $('#alert-message').addClass('alert-success');
-            $('#alert-message').text('Votre message a bien été envoyé');
             return true;
         }
     }
 }
 
 function Contact(){
-    const [message, setMessage] = useState('');
-    const [expediteur, setExpediteur] = useState('');
-    const [email, setEmail] = useState('');
-    const [status, setStatus] = useState('');
-    
-    const handleChangeMessage = event => {
-      setMessage(event.target.value);
-    };
-    
-    const handleChangeExpediteur = event => {
-      setExpediteur(event.target.value);
-    };
-    
-    const handleChangeEmail = event => {
-      setEmail(event.target.value);
-    };
-    
-    const handleSubmit = event => {
-        if(validateForm()) {
-            axios.post('http://localhost:8000/contact', {
-                expediteur: expediteur,
-                email: email,
-                message: message
-            })
-            .then(response => {
-            setStatus('success');
-            alert(response);
-            })
-            .catch(error => {
-            setStatus('error');
-            alert(error);
-            });
-        }
-    };
-
     return (
         <section id="contact">
             <h3>Contact</h3>
             <div className="parallax-container">
                 <div className="main container">
-                    <div id="alert-message" className="alert not-displayed">
-                        {/* render form elements */}
-                        {status === 'success' && <p>Your message was sent successfully!</p>}
-                        {status === 'error' && <p>Your message has thrown errors!</p>}
-                    </div>
+                    <div id="alert-message" className="alert not-displayed"></div>
                     <div className="row">
-                        <form method="post" id="contact-form" action="http://localhost:8000/contact" name="contact-form">
+                        <form method="post" action="/contact.php" id="contact-form" name="contact-form">
                             <div className="input-field">
 
-                                <label htmlFor="expediteur">Expéditeur</label>
-                                <input type="text" id="expediteur" name="expediteur" onChange={handleChangeExpediteur} value={expediteur} placeholder="John DOE" className="input-contact" required/>
+                                <label for="expediteur">Expéditeur</label>
+                                <input type="text" id="expediteur" name="expediteur" placeholder="John DOE" className="input-contact" required/>
 
-                                <label htmlFor="email">E-mail</label>
-                                <input type="email" id="email" name="email" onChange={handleChangeEmail} value={email} placeholder="john.doe@email.com" className="input-contact" required/>
+                                <label for="email">E-mail</label>
+                                <input type="email" id="email" name="email" placeholder="john.doe@email.com" className="input-contact" required/>
 
-                                <label htmlFor="message">Message</label>
-                                <textarea id="message" name="message" value={message} onChange={handleChangeMessage} placeholder="Bonjour Loïc, je vous contacte concernant..." rows="40" cols="50" required></textarea>
+                                <label for="message">Message</label>
+                                <textarea id="message" name="message" placeholder="Bonjour Loïc, je vous contacte concernant..." rows="40" cols="50" required></textarea>
                                 <br/>
-                                {/*<div className="g-recaptcha" data-sitekey="6LeCU9keAAAAAKv6LbiviEfv9t2IsHsOMbhmnzov"></div>
-                                <br/>*/}
+                                <div className="g-recaptcha" data-sitekey="6LeCU9keAAAAAKv6LbiviEfv9t2IsHsOMbhmnzov"></div>
+                                <br/>
 
-                                <input id="btnSubmit" onSubmit={handleSubmit} className="orange-background waves-effect waves-light btn" type="submit" value="Envoyer"/>
+                                <input id="btnSubmit" onSubmit={onFormSubmit()} className="orange-background waves-effect waves-light btn" type="submit" value="Envoyer"/>
                                 <a href="./cgu.html">Conditions d'utilisations</a>
                             </div>
                         </form>
