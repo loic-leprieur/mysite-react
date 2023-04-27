fc8f288524540394e333539c67b4149bc2718ef0
LOIC-PC-22\lleprieur <leprieurloic@gmail.com>
2023-04-28 01:12:24 +0200

    Changement de dossier et suppression du code de l'api déplacé dans un répôt dédié

---
+++ build/errors/notfound.php
@@ -0,0 +1,10 @@
+<?php require 'header.php';?>
+
+	<div class="container min-container-space center-align">
+		<h1 class="center-align">Erreur 404</h1>
+		<h3 class="center-align">Désolé, cette page n'existe pas...</h3>
+		<i class="large material-icons">error_outline</i>
+	</div>
+
+
+<?php require 'footer.php';?>
