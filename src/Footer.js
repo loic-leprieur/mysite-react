4eb08b685d0ad2c5d53449b6eac0b51486b8038d
loic-leprieur <leprieurloic@gmail.com>
2023-07-28 23:26:18 +0200

    Fix footer et date copyright dynamique

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,18 @@
 import React from 'react'
 
+function getCurrentYear() {
+  return new Date().getFullYear().toString()
+}
+
 function Footer() {
   return (
     <footer className="page-footer white darken-4">
-      <div className="container">
-        <div className="row">
-          <div className="col l6 s12">
-            <h5 className="black-text">Site web de Loïc LEPRIEUR</h5>
-            <p className="black-text">
-              Retrouvez tous mes travaux sur<a href="https://github.com/loic-leprieur"> GitHub</a>
-            </p>
-          </div>
-        </div>
-      </div>
       <div className="footer-copyright">
         <div className="container black-text">
-          <span>©2018-2022 - Loïc LEPRIEUR</span>
+          <span>©2018-{getCurrentYear()} - Loïc LEPRIEUR</span>
+          <p className="black-text">
+            Retrouvez tous mes travaux sur<a href="https://github.com/loic-leprieur"> GitHub</a>
+          </p>
           <a className="right" href="https://twitter.com/le_lowick">
             Twitter - @le_lowick
           </a>
